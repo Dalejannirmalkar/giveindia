b013789dc4377ef066abdde7f01866cab163c465
Dalejan Nirmalkar <dalejannirmalkar@gmail.com>
2019-07-28 22:18:28 +0530

    Fix logging and start up

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import helmet from "helmet";
 import * as bodyParser from "body-parser";
 import * as routes from "./routes";
 import { log } from "./middlewares/log";
@@ -8,7 +9,10 @@ import { realtimeStats } from "./middlewares/realtime-stats";
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+app.enable("trust proxy");
+app.disable("x-powered-by");
 
+app.use(helmet());
 app.use(express.static("public"));
 app.use(reqId());
 app.use(log());
--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -1,25 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import ms from "ms";
-import morgan, { TokenIndexer } from "morgan";
 import onHeaders from "on-headers";
-import { redisLogKeyPrefix } from "../constants";
-import { client as redisClient } from "../redis";
 import { statistics } from "../utils";
 import moment from "moment";
-
-function format(tokens: any, req: Request, res: Response) {
-  console.dir({ tokens });
-}
-
-morgan.token("duration", function getId(req: Request) {
-  return String(req.duration);
-});
-
-const _log = () =>
-  morgan("common", {
-    // @ts-ignore
-    stream: redisClient.writeStream(`${redisLogKeyPrefix}:`, ms("1w"))
-  });
 
 export const log = () => (req: Request, res: Response, next: NextFunction) => {
   statistics[req.method] = statistics[req.method] || [];
